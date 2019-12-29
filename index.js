d2a8c58b97ec3ac996afac0a170789c677396266
kawahara <kawahraa@b-style.net>
2019-12-29 20:03:21 +0900

    お試し

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ const alfy = require('alfy');
 const TextFixEngine = require("textlint").TextFixEngine;
 
 async function fixText(alfy_input) {
-    if (!alfy_input) {
-        alfy_input = "こんにちは嘘, 噂, 叶, 噛, 叩, 喋, 哨, 噌, 吊, 呆World"
-    }
-    const options = {
-        fix: true,
-        rules: ["no-todo"],
-        formatterName: "json"
-    };
-    const engine2 = new TextFixEngine(options);
-    console.log('sleep start');
-    const stringPromise = await engine2.executeOnText(alfy_input, ".txt").then(function (results) {
-        console.log('alfy_input:   ', alfy_input);
-        console.log('results: ', JSON.stringify(results));
-        const output = engine2.formatResults(results);
-        // stringPromise =alfy_input
-        return JSON.parse(output)[0].output
-    });
-    console.log('sleep end');
-    console.log('typeof', typeof stringPromise);
-    return stringPromise;
+    return "こんにちは嘘, 噂, 叶, 噛, 叩, 喋, 哨, 噌, 吊, 呆World"
+    // if (!alfy_input) {
+    //     alfy_input = "こんにちは嘘, 噂, 叶, 噛, 叩, 喋, 哨, 噌, 吊, 呆World"
+    // }
+    // const options = {
+    //     fix: true,
+    //     rules: ["no-todo"],
+    //     formatterName: "json"
+    // };
+    // const engine2 = new TextFixEngine(options);
+    // console.log('sleep start');
+    // const stringPromise = await engine2.executeOnText(alfy_input, ".txt").then(function (results) {
+    //     console.log('alfy_input:   ', alfy_input);
+    //     console.log('results: ', JSON.stringify(results));
+    //     const output = engine2.formatResults(results);
+    //     // stringPromise =alfy_input
+    //     return JSON.parse(output)[0].output
+    // });
+    // console.log('sleep end');
+    // console.log('typeof', typeof stringPromise);
+    // return stringPromise;
 
 }
 
