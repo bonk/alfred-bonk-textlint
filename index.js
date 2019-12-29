bf108bd9323238944a478d9ee43afe7d15f4d515
kawahara <kawahraa@b-style.net>
2019-12-29 16:37:24 +0900

    お試し

--- index.js
+++ index.js
@@ -4,20 +4,19 @@ const alfy = require('alfy');
 const TextFixEngine = require("textlint").TextFixEngine;
 const path = require("path");
 
-async function lintFile(input="こんにちは嘘, 噂, 叶, 噛, 叩, 喋, 哨, 噌, 吊, 呆World") {
+async function fixText(alfy_input = "こんにちは嘘, 噂, 叶, 噛, 叩, 喋, 哨, 噌, 吊, 呆World") {
     const options = {
         fix: true,
         rules: ["no-todo"],
         formatterName: "json"
     };
     const engine2 = new TextFixEngine(options);
-    const searchTarget = input;
     console.log('sleep start');
-    const stringPromise = await engine2.executeOnText(searchTarget, ".txt").then(function (results) {
-        console.log('input:   ',input);
-        console.log('results: ',JSON.stringify(results));
+    const stringPromise = await engine2.executeOnText(alfy_input, ".txt").then(function (results) {
+        console.log('alfy_input:   ', alfy_input);
+        console.log('results: ', JSON.stringify(results));
         const output = engine2.formatResults(results);
-        // stringPromise =searchTarget
+        // stringPromise =alfy_input
         return JSON.parse(output)[0].output
     });
     console.log('sleep end');
@@ -25,11 +24,11 @@ async function lintFile(input="こんにちは嘘, 噂, 叶, 噛, 叩, 喋, 哨,
 
 }
 
-lintFile(alfy.input).then(function (hoge) {
+fixText(alfy.input).then(function (fixedText) {
     alfy.output([
         {
             title: 'Unicorn',
-            hoge: hoge,
+            fixedText: fixedText,
             subtitle: alfy.input
         }
     ]);
