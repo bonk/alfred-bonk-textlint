67bb3e49d9c04cee8ff53b038bad74fc3d03ea81
kawahara <kawahraa@b-style.net>
2019-12-29 19:56:01 +0900

    お試し

--- index.js
+++ index.js
@@ -18,30 +18,18 @@ async function fixText(alfy_input = "こんにちは嘘, 噂, 叶, 噛, 叩, 喋
         return JSON.parse(output)[0].output
     });
     console.log('sleep end');
+    console.log('typeof',typeof stringPromise);
     return stringPromise;
 
 }
 
-// fixText(alfy.input).then(function (fixedText) {
-//     console.log('fixText start');
-//     alfy.output([
-//         {
-//             title: fixedText,
-//             subtitle: alfy.input
-//         }
-//     ]);
-//     console.log('fixText end');
-// });
-//
-// (async function() {
-//     let hoge = await fixText(alfy.input)
-// })();
-
-alfy.output([
-    {
-        title: (async function() {
-            return await fixText(alfy.input)
-        })(),
-        subtitle: alfy.input
-    }
-]);
+fixText(alfy.input).then(function (fixedText) {
+    console.log('fixText start');
+    alfy.output([
+        {
+            title: fixedText,
+            subtitle: alfy.input
+        }
+    ]);
+    console.log('fixText end');
+});
